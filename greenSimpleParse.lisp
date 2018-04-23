31d75c56c8832d906be25f091e3a63a938ffad9e
tmgreenside <trevmg7@gmail.com>
2018-04-22 20:28:19 -0700

    Added file functions

--- greenSimpleParse.lisp
+++ greenSimpleParse.lisp
@@ -2,18 +2,37 @@
 ; words starting with the letter 'm' or 'M'
 
 (defun myRead ()
-  (let (result "")
   (let ((in (open "./sampletext.txt" :if-does-not-exist nil)))
    (when in
       (loop for line = (read-line in nil)
-        while line do (concatenate 'string result item))
+        while line do (printLine line))
       (close in)
+      (write-line "")
    )
   )
-  result
 )
 
-(defvar x (myRead))
-(write-line "Here we go")
+(defun printLine (line)
+  (print line)
+)
 
-(write x)
+; given a list of strings, print to terminal and write to a file
+(defun myWrite (line)
+  (with-open-file (stream "myout.txt" :direction :output :if-exists :append :if-does-not-exist :create)
+    (format stream line)
+  )
+)
+
+(defun myClear (filename)
+  (with-open-file (stream filename :direction :output :if-exists :supersede))
+)
+
+(defun myWriteList (lines)
+  (with-open-file (stream "myout.txt" :direction :output :if-exists :append :if-does-not-exist :create)
+    (loop for line = (read-line lines)
+      while line do (printLine line)
+    )
+  )
+)
+
+(myWriteList "else")
